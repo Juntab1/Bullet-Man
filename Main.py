56d5122e30f59585f71cfca1d4f61ba2810dbb28
juntab1 <jsbwhaleguy@gmail.com>
2023-07-21 11:19:39 -0700

    standard state without global variable

--- Main.py
+++ Main.py
@@ -5,10 +5,16 @@ import random
 import time
 
 
-class scoreCount:
+class ScoreCount:
     score = 0
     def incr():
-        scoreCount.score += 1
+        ScoreCount.score += 1
+
+class Lives:
+    health = 3
+    def decr():
+        Lives.health -= 1
+
 
 class GameWindow:
     window = None
@@ -17,12 +23,12 @@ class GameWindow:
 def shoot(char_y,char_x):
     #score = 0
     for i in range(3):
-        if (char_x+i < width_window-1):
+        if (char_x+i < GameState.width_window-1):
             window.addch(char_y,char_x+i,'a')
             if (monster_y == char_y and monster_x == char_x+i):
                 curses.flash()
-                scoreCount.incr()
-                window.addstr(0,1,f"Score:{scoreCount.score}")
+                ScoreCount.incr()
+                window.addstr(0,1,f"Score:{ScoreCount.score}")
             window.refresh()
             time.sleep(0.5)
             window.addch(char_y,char_x+i,' ')
@@ -30,12 +36,12 @@ def shoot(char_y,char_x):
 def monster(char_y, char_x):
     global monster_y
     global monster_x
-    monster_y = random.randint(2,height_window-2)
-    monster_x = random.randint(2,width_window-2)
+    monster_y = random.randint(2,GameState.height_window-2)
+    monster_x = random.randint(2,GameState.width_window-2)
     if (monster_y == char_y):
-        monster_y = random.randint(monster_y+1, height_window-2)
+        monster_y = random.randint(monster_y+1, GameState.height_window-2)
     elif (monster_x == char_x):
-        monster_x = random.randint(monster_x+1, width_window-2)
+        monster_x = random.randint(monster_x+1, GameState.width_window-2)
     window.addch(monster_y,monster_x,'T')
 
 class GameState:
@@ -48,16 +54,15 @@ def main():
     curses.noecho()
     # respond without entering enter key
     curses.cbreak()
-    global height_window
-    global width_window
-    height_window = 6
-    width_window = 30
+    GameState.height_window = 6
+    GameState.width_window = 30
     char_x = 15
     char_y = 3
     global window 
-    window = curses.newwin(height_window,width_window)
+    window = curses.newwin(GameState.height_window,GameState.width_window)
     window.box()
-    window.addstr(0,1,f"Score:0")
+    window.addstr(0,9,"Score:0")
+    window.addstr(0,1,"Lives:3")
     curses.curs_set(0)
     monster(char_y,char_x)
     while True:
@@ -67,7 +72,7 @@ def main():
         window.addch(char_y,char_x,' ')
         window.refresh()
         if (c == ord('d')):
-            if (char_x < width_window-2):
+            if (char_x < GameState.width_window-2):
                 char_x += 1
             else:
                 curses.beep()
@@ -82,7 +87,7 @@ def main():
             else:
                 curses.beep()
         elif (c == ord('s')):
-            if (char_y < height_window-2):
+            if (char_y < GameState.height_window-2):
                 char_y += 1
             else:
                 curses.beep()
@@ -91,28 +96,19 @@ def main():
 
         elif (c == ord('q')):
             break
-
-
-
-
-
-
-
-    # refresh is needed to update the screen after you update variables
-    # window.refresh()
-
-    # can use curs_set(False) to suppress the blinking cursor
-    # getch() refreshes the screen and waits for the user to hit a key
-    # c = stdscr.getch() and c == ord(letter press)
-
-
-
-
     # end operation to finish a program
     curses.nocbreak()
     curses.echo()
     curses.endwin()
-  
+
 
 if __name__ == "__main__":
     main()
+
+
+# Comments:
+    # refresh is needed to update the screen after you update variables
+    # window.refresh()
+    # can use curs_set(False) to suppress the blinking cursor
+    # getch() refreshes the screen and waits for the user to hit a key
+    # c = stdscr.getch() and c == ord(letter press)
