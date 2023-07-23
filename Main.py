6bdc96819bc91411bcf503666a9ea6853da33d6e
juntab1 <jsbwhaleguy@gmail.com>
2023-07-23 16:33:58 -0700

    fixed parameters and how classes are called

--- Main.py
+++ Main.py
@@ -16,92 +16,90 @@ class Statistics:
 # the game
 class GameWindow:
     window = None
+    height_window = 6
+    width_window = 30
 
 # keeps track of variables that change frequently due to user choice, like user coordinate and etc.
 class GameState:
-    pass
+    char_x = 15
+    char_y = 3
 
 # function that decides if the user's gun reaches the enemy. If it does it updates the score variable
-def shoot():
-    temp_x = GameState.char_x+1
+def shoot(state, window, count):
+    temp_x = state.char_x+1
     for i in range(3):
-        if (temp_x+i < GameWindow.width_window-1):
-            GameWindow.window.addch(GameState.char_y,temp_x+i,'a')
-            if (GameState.monster_y == GameState.char_y and GameState.monster_x == temp_x+i):
+        if (temp_x+i < window.width_window-1):
+            window.window.addch(state.char_y,temp_x+i,'a')
+            if (state.monster_y == state.char_y and state.monster_x == temp_x+i):
                 curses.flash()
-                Statistics.incr()
-                GameWindow.window.addstr(0,9,f"Score:{Statistics.score}")
-            GameWindow.window.refresh()
+                count.incr()
+                window.window.addstr(0,9,f"Score:{count.score}")
+            window.window.refresh()
             time.sleep(0.5)
-            GameWindow.window.addch(GameState.char_y,temp_x+i,' ')
+            window.window.addch(state.char_y,temp_x+i,' ')
 
 # creates a monster at a random location on the map
-def monster():
-    GameState.monster_y = random.randint(2,GameWindow.height_window-2)
-    GameState.monster_x = random.randint(2,GameWindow.width_window-2)
-    if (GameState.monster_y == GameState.char_y):
-        GameState.monster_y = random.randint(GameState.monster_y+1, GameWindow.height_window-2)
-    elif (GameState.monster_x == GameState.char_x):
-        GameState.monster_x = random.randint(GameState.monster_x+1, GameWindow.width_window-2)
-    GameWindow.window.addch(GameState.monster_y,GameState.monster_x,'T')
+def monster(state, window):
+    state.monster_y = random.randint(2,window.height_window-2)
+    state.monster_x = random.randint(2,window.width_window-2)
+    if (state.monster_y == state.char_y):
+        state.monster_y = random.randint(state.monster_y+1, window.height_window-2)
+    elif (state.monster_x == state.char_x):
+        state.monster_x = random.randint(state.monster_x+1, window.width_window-2)
+    window.window.addch(state.monster_y,state.monster_x,'T')
 
 
-def main():
+def main(state, window_info, count):
     curses.initscr()
     # disable reading keys to the screen
     curses.noecho()
     # respond without entering enter key
     curses.cbreak()
-    GameWindow.height_window = 6
-    GameWindow.width_window = 30
-    GameState.char_x = 15
-    GameState.char_y = 3
-    GameWindow.window = curses.newwin(GameWindow.height_window,GameWindow.width_window)
-    GameWindow.window.box()
-    GameWindow.window.addstr(0,9,"Score:0")
-    GameWindow.window.addstr(0,1,"Lives:3")
+    window_info.window = curses.newwin(window_info.height_window,window_info.width_window)
+    window_info.window.box()
+    window_info.window.addstr(0,9,"Score:0")
+    window_info.window.addstr(0,1,"Lives:3")
     curses.curs_set(0)
-    monster()
+    monster(GameState,GameWindow)
     while True:
-        GameWindow.window.addch(GameState.char_y,GameState.char_x,'A')
-        if (GameState.monster_x == GameState.char_x and GameState.monster_y == GameState.char_y):
-            Statistics.decr()
-            GameWindow.window.addstr(0,1,f"Lives:{Statistics.health}")
-            GameWindow.window.refresh()
-            if (Statistics.health == 0):
-                GameWindow.window.addstr(2,10,f"YOU LOSE!")
-                GameWindow.window.refresh()
+        window_info.window.addch(state.char_y,state.char_x,'A')
+        if (state.monster_x == state.char_x and state.monster_y == state.char_y):
+            count.decr()
+            window_info.window.addstr(0,1,f"Lives:{count.health}")
+            window_info.window.refresh()
+            if (count.health == 0):
+                window_info.window.addstr(2,10,f"YOU LOSE!")
+                window_info.window.refresh()
                 time.sleep(1.5)
                 exit()
-        c = GameWindow.window.getch()
-        GameWindow.window.addch(GameState.char_y,GameState.char_x,' ')
-        GameWindow.window.refresh()
+        c = window_info.window.getch()
+        window_info.window.addch(state.char_y,state.char_x,' ')
+        window_info.window.refresh()
         if (c == ord('d')):
-            if (GameState.char_x < GameWindow.width_window-2):
-                GameState.char_x += 1
+            if (state.char_x < window_info.width_window-2):
+                state.char_x += 1
             else:
                 curses.beep()
         elif (c == ord('a')):
-            if (GameState.char_x > 1):
-                GameState.char_x -= 1
+            if (state.char_x > 1):
+                state.char_x -= 1
             else:
                 curses.beep()
         elif (c == ord('w')):
-            if (GameState.char_y > 1):
-                GameState.char_y -= 1
+            if (state.char_y > 1):
+                state.char_y -= 1
             else:
                 curses.beep()
         elif (c == ord('s')):
-            if (GameState.char_y < GameWindow.height_window-2):
-                GameState.char_y += 1
+            if (state.char_y < window_info.height_window-2):
+                state.char_y += 1
             else:
                 curses.beep()
         elif (c == ord(' ')):
-            shoot()
+            shoot(GameState, GameWindow, Statistics)
             # if (GameState.hit == True):
             #     monster()
             # GameState.hit = False
-
         elif (c == ord('q')):
             break
     # lines that end operation to finish a program
@@ -111,7 +109,7 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    main(GameState,GameWindow,Statistics)
 
 
 # Comments:
