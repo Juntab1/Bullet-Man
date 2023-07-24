92eb520c6fa3d5ac916e756a6ec0b30e8235b7f1
juntab1 <jsbwhaleguy@gmail.com>
2023-07-24 16:17:33 -0700

    render fixed to prepare for world implementation

--- Main.py
+++ Main.py
@@ -2,24 +2,22 @@ import curses
 import random
 import time
 
-# keeps track of universal score and health.
-class Statistics:
+# renders the window we are currently are at
+class Render:
     def __init__(self):
-        self.score = 0
-        self.health = 3
-    def incr(self):
-        self.score += 1
-    def decr(self):
-        self.health -= 1
-    
+        pass
+    def render(self, state, monster):
+        window = state.window
+        stats = state.stats
+        player = state.world.player
+        window.clear()
+        window.box()
+        stats.render(state)
+        player.render(state)
+        monster.render(state)
+        window.refresh()
 
-# keeps track of any variable relating to the window of the game.
-class GameWindow:
-    def __init__(self):
-        self.window = None
-        self.height_window = 6
-        self.width_window = 30
-
+# keeps track of player position on world
 class Player:
     def __init__(self, start_x, start_y):
         self.start_x = start_x
@@ -27,8 +25,8 @@ class Player:
         self.x = start_x
         self.y = start_y
 
-    def render(self, window_info):
-        window_info.window.addch(self.y, self.x, 'A')
+    def render(self, state):
+        state.window.addch(self.y, self.x, 'A')
 
 # keeps track of the game world in a 2d grid
 class World:
@@ -39,120 +37,160 @@ class World:
     
 # keeps track of variables that change frequently due to user choice, like user coordinate and etc.
 class GameState:
-    def __init__(self, window_max_x, window_max_y):
+    # pass in 30 for x and 6 for y  
+    def __init__(self, window_max_y, window_max_x):
         # TODO: starting with a world whose size is the same as the window, but later it should
         # be made bigger (so we can move around)
         self.world = World(15, 3, window_max_x, window_max_y)
-        self.hit = False
+        self.window = None
+        self.stats = Statistics()
+    def reg_operation(self, monster, render):
+        while True:
+            player = self.world.player
+            if (monster.x == player.x and monster.y == player.y):
+                # can we pass in self like this?
+                self.stats.lives_state(monster, self, render)
+            # TODO - can we clear the window and re-draw everything at current position
+            # instead of trying the complicated dance of setting their pos to " "
+            render.render(self, monster)
+            # read input
+            window = self.window
+            world = self.world
+            # read input
+            c = window.getch()
+            if (c == ord('d')):
+                if (player.x < self.world.max_x - 2):
+                    player.x += 1
+                else:
+                    curses.beep()
+            elif (c == ord('a')):
+                if (player.x > 1):
+                    player.x -= 1
+                else:
+                    curses.beep()
+            elif (c == ord('w')):
+                if (player.y > 1):
+                    player.y -= 1
+                else:
+                    curses.beep()
+            elif (c == ord('s')):
+                if (player.y < self.world.max_y - 2):
+                    player.y += 1
+                else:
+                    curses.beep()
+            elif (c == ord(' ')):
+                bullet = Bullet(player.y, player.x)
+                bullet.shoot(self,monster,render)
+            elif (c == ord('q')):
+                break
 
-# function that decides if the user's gun reaches the enemy. If it does it updates the score variable.
-def shoot_pistol(state, window_info, count):
-    player = state.world.player
-    temp_x = (state.world.player.x + 1)
-    for i in range(3):
-        if ((temp_x + i) < (window_info.width_window - 1)):
+# stats of the user in the game
+class Statistics:
+    def __init__(self):
+        self.score = 0
+        self.health = 3
+    def incr_score(self):
+        self.score += 1
+    def decr_health(self):
+        self.health -= 1
+    def lives_state(self, monster, state, render):
+        window = state.window
+        self.decr_health()
+        if (self.health != 0):
+            monster.create_monster(state)
+        window.addstr(0,1,f"Lives:{self.health}")
+        window.refresh()
+        if (self.health == 0):
+            render.render(state,monster)
+            window.addstr(2, 10, f"YOU LOSE!")
+            window.refresh()
+            time.sleep(1.5)
+            exit()
+    def render(self, state):
+        window = state.window
+        window.addstr(0,9,f"Score:{self.score}")
+        window.addstr(0,1,f"Lives:{self.health}")
 
-            #TODO - would be great to replace the 'bullet' with a class
-            # that is unrelated to the player and maintain's it's own x, and y.
-            # then we can have a bullet.render() on that class and disconnect
-            # it from the position of the player in this function.
-            window_info.window.addch(player.y, temp_x + i, 'a')
-            if (state.monster_y == player.y and state.monster_x == (temp_x + i)):
-                curses.flash()
-                count.incr()
-                window_info.window.addstr(0, 9, f"Score:{count.score}")
-                state.hit = True
-            window_info.window.refresh()
-            time.sleep(0.5)
-            window_info.window.addch(player.y, temp_x + i, ' ')
+# bullet operations of the user
+class Bullet:
+    def __init__(self,start_y,start_x):
+        self.x = start_x
+        self.y = start_y
+    
+    def render(self, state):
+        state.window.addch(self.y, self.x, 'a')
 
-# creates a monster at a random location on the map
-def create_monster(state, window_info):
-    player = state.world.player
-    state.monster_y = random.randint(2, window_info.height_window - 2)
-    state.monster_x = random.randint(2, window_info.width_window - 2)
-    if (state.monster_y == player.y):
-        state.monster_y = random.randint(1, window_info.height_window - 2)
-        while (state.monster_y == player.y):
-            state.monster_y = random.randint(1, window_info.height_window - 2)
-    elif (state.monster_x == player.x):
-        state.monster_x = random.randint(1, window_info.width_window - 2)
-        while (state.monster_x == player.x):
-            state.monster_x = random.randint(1, window_info.width_window - 2)
-    window_info.window.addch(state.monster_y, state.monster_x, 'T')
+    # need to delete this later
+    def shoot(self, state, monster, render):
+        player = state.world.player
+        world = state.world
+        stats = state.stats
+        temp_x = (state.world.player.x + 1)
+        for i in range(3):
+            if ((temp_x + i) < (world.max_x - 1)):
+                self.x = temp_x + i
+                #TODO - would be great to replace the 'bullet' with a class
+                # that is unrelated to the player and maintain's it's own x, and y.
+                # then we can have a bullet.render() on that class and disconnect
+                # it from the position of the player in this function.
+                
+                # do render here instead of doing the window.addch
+                self.render(state)
+                if (monster.y == self.y and monster.x == self.x):
+                    monster.create_monster(state)
+                    curses.flash()
+                    stats.incr_score()
+                state.window.refresh()
+                time.sleep(0.5)
+                render.render(state,monster)
+
+class Monster:
+    def __init__(self):
+        self.y = 5
+        self.x = 5
+
+    # creates a monster at a random location on the map
+    def create_monster(self, state):
+        world = state.world
+        player = state.world.player
+        self.y = random.randint(2, world.max_y - 2)
+        self.x = random.randint(2, world.max_x - 2)
+        if (self.y == player.y):
+            self.y = random.randint(1, world.max_y - 2)
+            while (self.y == player.y):
+                self.y = random.randint(1, world.max_y - 2)
+        elif (self.x == player.x):
+            self.x = random.randint(1, world.max_x - 2)
+            while (self.x == player.x):
+                self.x = random.randint(1, world.max_x - 2)
+    
+    # renders the monster on the screen
+    def render(self, state):
+        state.window.addch(self.y, self.x, 'T')
+    # this is going to be nessesary for when we move the monster toward the player
+    # def move_monster(self, state, window_info):
+    #     if ((self.monster_y == state.char_y)):
+    #         for i in range(2):
+    #             if (self.monster_y > 1 and self.monster_y < window_info.height_window - 2):
+    #                 self.monster_y 
+    #     elif ((self.monster_x == state.char_x)):
 
 
 def main():
-    window_info = GameWindow()
-    game_state = GameState(window_info.height_window, window_info.width_window)
-    stats_info = Statistics()
-
     curses.initscr()
     curses.noecho()
     curses.cbreak()
-
-    window_info.window = curses.newwin(window_info.height_window, window_info.width_window)
-    window_info.window.box()
-    window_info.window.addstr(0,9,"Score:0")
-    window_info.window.addstr(0,1,"Lives:3")
-    
     curses.curs_set(0)
-    create_monster(game_state, window_info)
-
-    game_state.world.player.render(window_info)
-    
+    # later have to put world length inside of it instead of just window
+    game_state = GameState(6, 30)
+    game_state.window = curses.newwin(6, 30)
+    player = game_state.world.player
+    render = Render()
+    monster = Monster()
+    monster.create_monster(game_state)
+    render.render(game_state, monster)
     # main game loop
-    while True:
-        player = game_state.world.player
-
-        if (game_state.monster_x == player.x and game_state.monster_y == player.y):
-            stats_info.decr()
-            if (stats_info.health != 0):
-                create_monster(game_state, window_info)
-            window_info.window.addstr(0,1,f"Lives:{stats_info.health}")
-            window_info.window.refresh()
-            if (stats_info.health == 0):
-                window_info.window.addstr(2, 10, f"YOU LOSE!")
-                window_info.window.refresh()
-                time.sleep(1.5)
-                exit()
-        
-        # read input
-        c = window_info.window.getch()
-
-        # TODO - can we clear the window and re-draw everything at current position
-        # instead of trying the complicated dance of setting their pos to " "
-
-        window_info.window.addch(player.y, player.x, ' ')
-        window_info.window.refresh()
-        if (c == ord('d')):
-            if (player.x < window_info.width_window - 2):
-                player.x += 1
-            else:
-                curses.beep()
-        elif (c == ord('a')):
-            if (player.x > 1):
-                player.x -= 1
-            else:
-                curses.beep()
-        elif (c == ord('w')):
-            if (player.y > 1):
-                player.y -= 1
-            else:
-                curses.beep()
-        elif (c == ord('s')):
-            if (player.y < window_info.height_window - 2):
-                player.y += 1
-            else:
-                curses.beep()
-        elif (c == ord(' ')):
-            shoot_pistol(game_state, window_info, stats_info)
-            if (game_state.hit == True):
-                create_monster(game_state, window_info)
-            game_state.hit = False
-        elif (c == ord('q')):
-            break
-        player.render(window_info)
+    game_state.reg_operation(monster, render)
     curses.nocbreak()
     curses.echo()
     curses.endwin()
